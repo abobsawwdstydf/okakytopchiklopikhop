77a5657ddf2244235af5177c93ec5bc276318149
abobsawwdstydf <zamakinarsenijcom@gmail.com>
2025-10-26 15:11:48 +0700

    Add files via upload

--- activate_this.py
+++ activate_this.py
@@ -169,4 +169,4 @@ def home():
     """
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=0, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=71203, debug=True, threaded=True)
